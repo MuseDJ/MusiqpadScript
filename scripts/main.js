508fef72b6486752eda4ecf95e8207da34924dcb
itotallyrock <itotallyrock@users.noreply.github.com>
2016-02-21 17:34:09 -0600

    Added Base Functionality for AutoLike

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,28 @@ var muse = {
     VERSION: "0.0.0",
     NAME: "MuseScript",
     AUTHOR: "MuseDJ",
-    prefix: "[" + this.NAME + this.VERSION + "]",
+    prefix: "[" + this.NAME + " " + this.VERSION + "]" | "[MuseDJ]",
+    settings: {
+        autowoot: true,
+        theme: "",
+        verbose: 4
+    },
     fn: {
         logger: {
             log: function(msg, options) {
+                if(options.verbose)
+                    if(muse.settings.verbose >= options.verbose)
+                        console.log(muse.prefix, msg);
                 console.log(muse.prefix, msg);
-            }
+            },
         },
         init: function() {
             muse.fn.logger.log("Started.");
-        }
+            API.on(API.DATA.EVENTS.DJ_QUEUE_CYCLE, function(event) {
+                muse.fn.logger.log("Liking Current Song", {
+                    verbose: 2
+                });
+            });
+        },
     },
 };
