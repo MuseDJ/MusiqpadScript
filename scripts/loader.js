1835674f2b2b7133977c77e632146fef08731cca
itotallyrock <itotallyrock@users.noreply.github.com>
2016-02-26 16:16:32 -0800

    Fixed loader

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,9 +1,11 @@
 $.getScript("https://rawgit.com/MuseDJ/MusiqpadScript/master/scripts/main.js");
+var startTime = Date.now();
 var museLoader = setInterval(function() {
-    if(API !== undefined)
-        if(muse !== undefined)
+    if (typeof API !== "undefined")
+        if (typeof muse !== "undefined")
             setTimeout(function() {
                 muse.fn.init();
+                muse.fn.logger.info("Initialized in " + Date.now() - startTime);
                 clearInterval(museLoader);
             }, 500);
 }, 500);
