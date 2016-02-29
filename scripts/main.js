c3fc021f359f33f04fb3d7e218bcdec8c75f2203
itotallyrock <itotallyrock@users.noreply.github.com>
2016-02-28 16:26:24 -0800

    Added tick and fixed ETA

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 window.muse = {
-    VERSION: "0.0.0",
+    VERSION: "0.1.0",
     NAME: "MuseScript",
     AUTHOR: "MuseDJ",
     prefix: "[MuseDJ]",
@@ -8,35 +8,56 @@ window.muse = {
         theme: "",
         verbose: 4
     },
+    tick: {
+        rate: 10,
+        number: 0,
+        tick: undefined
+    },
     fn: {
         logger: {
             log: function(msg, options) {
-                if (options !== undefined)
-                    if (options.verbose !== undefined)
-                        if (muse.settings.verbose >= options.verbose)
+				if (options !== undefined) {
+                    if (options.verbose !== undefined) {
+                        if (muse.settings.verbose >= options.verbose) {
                             console.log(muse.prefix, msg);
-                console.log(muse.prefix, msg);
+						}
+					}
+				} else {
+					console.log(muse.prefix, msg);
+				}
             },
             error: function(msg, options) {
-                if (options !== undefined)
-                    if (options.verbose !== undefined)
-                        if (muse.settings.verbose >= options.verbose)
+				if (options !== undefined) {
+                    if (options.verbose !== undefined) {
+                        if (muse.settings.verbose >= options.verbose) {
                             console.error(muse.prefix, msg);
-                console.error(muse.prefix, msg);
+						}
+					}
+				} else {
+					console.error(muse.prefix, msg);
+				}
             },
             warn: function(msg, options) {
-                if (options !== undefined)
-                    if (options.verbose !== undefined)
-                        if (muse.settings.verbose >= options.verbose)
+				if (options !== undefined) {
+                    if (options.verbose !== undefined) {
+                        if (muse.settings.verbose >= options.verbose) {
                             console.warn(muse.prefix, msg);
-                console.warn(muse.prefix, msg);
+						}
+					}
+				} else {
+					console.warn(muse.prefix, msg);
+				}
             },
             info: function(msg, options) {
-                if (options !== undefined)
-                    if (options.verbose !== undefined)
-                        if (muse.settings.verbose >= options.verbose)
+                if (options !== undefined) {
+                    if (options.verbose !== undefined) {
+                        if (muse.settings.verbose >= options.verbose) {
                             console.info(muse.prefix, msg);
-                console.info(muse.prefix, msg);
+						}
+					}
+				} else {
+					console.info(muse.prefix, msg);
+				}
             }
         },
         chat: {
@@ -55,7 +76,7 @@ window.muse = {
                 console.log(raw);
                 console.log($.parseHTML(raw));
                 $('#messages').append(raw);
-				$('#chat').scrollTop($('#chat')[0].scrollHeight);
+                $('#chat').scrollTop($('#chat')[0].scrollHeight);
             },
             log: function(msg, options) {
                 API.chat.log("<span class=\"muse-log\">" + msg + "</span>", muse.NAME);
@@ -73,15 +94,46 @@ window.muse = {
                 API.chat.log("<span class=\"muse-success\">" + msg + "</span>", muse.NAME);
             },
         },
-		utils: {
-			makeTime: function(dateObj) {
-	            var settings = JSON.parse(localStorage.settings);
-	            if (settings.roomSettings && settings.roomSettings.chatTimestampFormat == API.DATA.CHAT.TSFORMAT.HR12)
-	                return ((dateObj.getHours()) % 12 || 12) + ':' + ('0' + dateObj.getMinutes()).slice(-2);
-	            else
-	                return (dateObj.getHours() + ':' + ('0' + dateObj.getMinutes()).slice(-2));
-	        }
-		},
+        utils: {
+            makeTime: function(dateObj) {
+                var settings = JSON.parse(localStorage.settings);
+                if (settings.roomSettings && settings.roomSettings.chatTimestampFormat == API.DATA.CHAT.TSFORMAT.HR12)
+                    return ((dateObj.getHours()) % 12 || 12) + ':' + ('0' + dateObj.getMinutes()).slice(-2);
+                else
+                    return (dateObj.getHours() + ':' + ('0' + dateObj.getMinutes()).slice(-2));
+            },
+            makeTimeFromMs: function(ms) {
+                ms = Math.floor(ms);
+                var hours = Math.floor(ms / (60 * 60 * 1000)),
+                    mins = Math.floor((ms - (hours * 60 * 60 * 1000)) / (60 * 1000)),
+                    secs = Math.floor((ms - (hours * 60 * 60 * 1000) - (mins * 60 * 1000)) / (1000));
+
+                hours = (hours < 10) ? "0" + hours : hours;
+                mins = (mins < 10) ? "0" + mins : mins;
+                secs = (secs < 10) ? "0" + secs : secs;
+
+                return hours + ":" + mins + ":" + secs;
+            }
+        },
+        tick: function() {
+            var startTime = Date.now();
+
+			muse.fn.logger.info("Current ETA " + muse.fn.utils.makeTimeFromMs(muse.fn.getETA()*1000), {
+                verbose: 5
+            });
+
+			$('.muse-time').text(muse.fn.utils.makeTimeFromMs(muse.fn.getETA()*1000));
+
+            muse.fn.logger.info("Tick #" + muse.tick.number + " took " + API.util.timeConvert(Date.now(), startTime).milisseconds, {
+                verbose: 5
+            });
+            if (API.util.timeConvert(Date.now(), startTime).milisseconds > 1 / (muse.tick.rate) * 1000) {
+                muse.fn.logger.error("Tick #" + muse.tick.number + " took longer than expected, stopping " + muse.name, {
+                    verbose: 0
+                });
+                muse.fn.end("Tick took to long.");
+            }
+        },
         loadCSS: function(url, callback) {
             $('head').append(
                 $('<link rel="stylesheet" type="text/css" />').attr('href', encodeURI(url))
@@ -99,6 +151,13 @@ window.muse = {
                     muse.fn.logger.log("Loaded CSS for " + url + ".");
                 }
             });
+
+            muse.tick.tick = setInterval(function() {
+                muse.fn.tick();
+                muse.tick.number++;
+            }, 1 / (muse.tick.rate / 1000));
+
+			$('.btn-join').append('<div class="muse-time">'+"00:00:00"+'</div>')
 
             API.on(API.DATA.EVENTS.ADVANCE, function(event) {
                 muse.fn.logger.log("Song Update", {
@@ -126,19 +185,27 @@ window.muse = {
 
             muse.fn.initCmds();
 
-			muse.fn.chat.raw({msg: "Has launched successfully",icon:"music-note",class:"muse-success"});
+            muse.fn.chat.raw({
+                msg: "Has launched successfully",
+                icon: "music-note",
+                class: "muse-success"
+            });
         },
         getETA: function() {
-            var totalSong = 0,
+            var total = 0,
                 avgSong = 1;
             API.room.getHistory().forEach(function(o, i) {
-                totalSong += o.song.duration;
+                total += o.song.duration;
             });
-            avgSong = totalSong / API.room.getHistory().length;
+            avgSong = total / API.room.getHistory().length;
             if (API.queue.getPosition(API.room.getUser().uid) == 1) {
                 return
             } else if (API.queue.getDJ().uid == API.room.getUser().uid) {
-                return avgSong * API.queue.getPosition(API.room.getUser().uid);
+                return Math.ceil(avgSong * API.queue.getPosition(API.room.getUser().uid) + +API.room.getTimeRemaining());
+            } else if (API.queue.getPosition(API.room.getUser().uid) == -1) {
+                return Math.ceil(avgSong * API.queue.getInfo().length + API.room.getTimeRemaining());
+            } else {
+                return Math.abs(Math.max())
             }
         },
         end: function(err) {
@@ -149,6 +216,9 @@ window.muse = {
             }
 
             muse.fn.endCmds();
+            clearInterval(muse.tick.tick);
+
+			$('.muse-time').remove();
 
             if (err) {
                 muse.fn.logger.error(err);
